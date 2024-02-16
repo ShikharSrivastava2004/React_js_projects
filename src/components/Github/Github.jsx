e61a4cc641b5446e4620c7b5d33ca8904a736b86
Shikhar Srivastava <143494296+ShikharSrivastava2004@users.noreply.github.com>
2024-02-16 12:52:33 +0530

    first commit

---
+++ src/components/Github/Github.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { useLoaderData } from 'react-router-dom'
+function Github() {
+    const data = useLoaderData()
+  return (
+    <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl'>Github followers:{data.followers}
+    <img src= {data.avatar_url} alt="GitHub Picture" width ={300}/>
+    </div>
+  )
+}
+
+export default Github
+
+export const githubInfoLoader= async() =>{
+    const response = await fetch('https://api.github.com/users/ShikarSrivastava2004')
+    return response.json()
+}
